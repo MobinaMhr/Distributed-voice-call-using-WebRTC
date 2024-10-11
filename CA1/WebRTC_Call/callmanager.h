634ad832648eefc34e1d5f733fa92eb1510c2c28
Mohammadreza <mrmh13801225@gmail.com>
2024-10-12 01:26:21 +0330

    Initial implementation of CallManager header file

--- CA1/WebRTC_Call/callmanager.h
+++ CA1/WebRTC_Call/callmanager.h
@@ -2,14 +2,26 @@
 #define CALLMANAGER_H
 
 #include <QObject>
+#include <QString>
 
 class CallManager : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString callerId READ callerId WRITE setCallerId NOTIFY callerIdChanged)
+
 public:
     explicit CallManager(QObject *parent = nullptr);
 
+    QString callerId() const;
+
+    void setCallerId(const QString &callerId);
+
+    Q_INVOKABLE void startCall();
+
+    Q_INVOKABLE void endCall();
+
 signals:
+    QString m_callerId;
 };
 
 #endif // CALLMANAGER_H
