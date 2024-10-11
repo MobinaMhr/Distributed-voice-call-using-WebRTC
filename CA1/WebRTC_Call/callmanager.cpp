494b4bfa15aeec087077abd2bbaa92b5b49c8d6b
Mohammadreza <mrmh13801225@gmail.com>
2024-10-12 01:34:10 +0330

    initial implementation of CallManager cpp file

--- CA1/WebRTC_Call/callmanager.cpp
+++ CA1/WebRTC_Call/callmanager.cpp
@@ -1,5 +1,34 @@
 #include "callmanager.h"
+#include <QDebug>
 
 CallManager::CallManager(QObject *parent)
-    : QObject{parent}
-{}
+    : QObject{parent},
+m_ipAddress("172.16.142.176"),
+m_iceCandidate("172.16.142.176")
+{
+}
+
+QString CallManager::callerId() const
+{
+    return m_callerId;
+}
+
+void CallManager::setCallerId(const QString &callerId)
+{
+    if (m_callerId != callerId) {
+        m_callerId = callerId;
+        emit callerIdChanged();
+    }
+}
+
+void CallManager::startCall()
+{
+    qDebug() << "Starting call with Caller ID:" << m_callerId;
+}
+
+void CallManager::endCall()
+{
+    qDebug() << "Ending call with Caller ID:" << m_callerId;
+}
+
+
