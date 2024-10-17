7851acf427bad9a8df49b876232fbffaa3064ac8
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-10-17 13:07:10 -0700

    Send answer message to target user

--- V3/index.js
+++ V3/index.js
@@ -41,6 +41,16 @@ ws.on("request", (req) => {
 
                 case "answer":
                     console.log(`Received an answer from ${data.user} to connect to : ${data.target}`)
+                    targetUser = peers[data.target]
+                    if (targetUser) {
+                        targetUser.send(JSON.stringify({
+                            type: "answer",
+                            user: data.user,
+                            sdp: data.sdp,
+                            iceCandidate: data.iceCandidate
+                        }))
+                    }
+
                     break
         }
 
