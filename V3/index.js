4b01aaa519be5bfec73a39072d5abeb1e7ffd56e
Mohammadreza <mrmh13801225@gmail.com>
2024-10-17 22:38:42 +0330

    implement the signaling server configurations:

---
+++ V3/index.js
@@ -0,0 +1,11 @@
+const http = require("http")
+const Socket = require("websocket").server
+const { v4: uuidv4 } = require("uuid");
+
+const server = http.createServer(() => {})
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
+
+const ws = new Socket({ httpServer: server })
