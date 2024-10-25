03c79e3a3d4fe5cf711d0558b2a9e895a2b22b0a
Mohammadreza <mrmh13801225@gmail.com>
2024-10-25 22:58:00 +0330

    implement the AudioOutput constructor

--- CA1/WebRTC_Call/audiooutput.cpp
+++ CA1/WebRTC_Call/audiooutput.cpp
@@ -1,5 +1,16 @@
 #include "audiooutput.h"
 
 AudioOutput::AudioOutput(QObject *parent)
-    : QObject{parent}
-{}
+    : QObject(parent), audioSink(nullptr), opusDecoder(nullptr), audioDevice(nullptr) {
+    int sampleRate = 48000;
+    int channelCount = 1;
+
+    QAudioFormat audioFormat;
+    audioFormat.setSampleRate(sampleRate);         // 48kHz
+    audioFormat.setChannelCount(channelCount);     // Mono audio
+    audioFormat.setSampleFormat(QAudioFormat::Int16);
+
+    if (!QMediaDevices::defaultAudioOutput().isFormatSupported(audioFormat)) {
+        qDebug() << "This audio format is not supported.";
+    }
+}
