fca04998203f407c327ca2d54eb18644795361de
Mohammadreza <mrmh13801225@gmail.com>
2024-10-25 22:34:47 +0330

    implement the initial version of audio output header file

--- CA1/WebRTC_Call/audiooutput.h
+++ CA1/WebRTC_Call/audiooutput.h
@@ -2,14 +2,36 @@
 #define AUDIOOUTPUT_H
 
 #include <QObject>
+#include <QAudioSink>
+#include <QAudioFormat>
+#include <QByteArray>
+#include <QMediaDevices>
+#include <QMutex>
+#include <QQueue>
+#include <opus.h>
+#include <QDebug>
 
 class AudioOutput : public QObject
 {
     Q_OBJECT
 public:
     explicit AudioOutput(QObject *parent = nullptr);
+    ~AudioOutput();
+
+    void addData(const QByteArray &data);
 
 signals:
+    void newPacket();
+
+private slots:
+    void play();
+
+private:
+    QAudioSink *audioSink;
+    OpusDecoder *opusDecoder;
+    QIODevice *audioDevice;
+    QMutex mutex;
+    QQueue<QByteArray> audioQueue;
 };
 
 #endif // AUDIOOUTPUT_H
