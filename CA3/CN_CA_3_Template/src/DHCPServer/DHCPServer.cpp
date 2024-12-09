f34fb6f7e6fe70ab3eb752014bdabdee90859df5
Mohammadreza <mrmh13801225@gmail.com>
2024-12-09 21:03:54 +0330

    initial branch commit

--- CA3/CN_CA_3_Template/src/DHCPServer/DHCPServer.cpp
+++ CA3/CN_CA_3_Template/src/DHCPServer/DHCPServer.cpp
@@ -1,9 +1,9 @@
-#include "DHCPServer.h"
+// #include "DHCPServer.h"
 
-DHCPServer::DHCPServer(QObject *parent) :
-    QObject {parent}
-{
-    connect(this, &DHCPServer::sendOffer, this, &DHCPServer::sendPacket);
-}
+// DHCPServer::DHCPServer(QObject *parent) :
+//     QObject {parent}
+// {
+//     connect(this, &DHCPServer::sendOffer, this, &DHCPServer::sendPacket);
+// }
 
-DHCPServer::~DHCPServer() {}
+// DHCPServer::~DHCPServer() {}
--- CA3/CN_CA_3_Template/src/Port/Port.h
+++ CA3/CN_CA_3_Template/src/Port/Port.h
@@ -1,32 +1,32 @@
-#ifndef PORT_H
-#define PORT_H
+// #ifndef PORT_H
+// #define PORT_H
 
-#include "../Packet/Packet.h"
+// #include "../Packet/Packet.h"
 
-#include <QObject>
+// #include <QObject>
 
-class Port : public QObject
-{
-    Q_OBJECT
+// class Port : public QObject
+// {
+//     Q_OBJECT
 
-public:
-    explicit Port(QObject *parent = nullptr);
-    ~Port() override;
+// public:
+//     explicit Port(QObject *parent = nullptr);
+//     ~Port() override;
 
-Q_SIGNALS:
-    void packetSent(const PacketPtr_t &data);
-    void packetReceived(const PacketPtr_t &data);
+// Q_SIGNALS:
+//     void packetSent(const PacketPtr_t &data);
+//     void packetReceived(const PacketPtr_t &data);
 
-public Q_SLOTS:
-    void sendPacket(const PacketPtr_t &data);
-    void receivePacket(const PacketPtr_t &data);
+// public Q_SLOTS:
+//     void sendPacket(const PacketPtr_t &data);
+//     void receivePacket(const PacketPtr_t &data);
 
-private:
-    uint8_t  m_number;
-    uint64_t m_numberOfPacketsSent;
-    QString  m_routerIP;
-};
+// private:
+//     uint8_t  m_number;
+//     uint64_t m_numberOfPacketsSent;
+//     QString  m_routerIP;
+// };
 
-typedef QSharedPointer<Port> PortPtr_t;
+// typedef QSharedPointer<Port> PortPtr_t;
 
-#endif    // PORT_H
+// #endif    // PORT_H
