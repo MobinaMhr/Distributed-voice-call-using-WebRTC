cfe9e51d773a578c018488eba65262ca39b14485
Mohammadreza <mrmh13801225@gmail.com>
2024-12-10 16:38:07 +0330

    implement macAddres string based constructor

--- CA3/CN_CA_3_Template/src/MACAddress/macAddress.cpp
+++ CA3/CN_CA_3_Template/src/MACAddress/macAddress.cpp
@@ -4,3 +4,24 @@ MacAddress::MacAddress(uint64_t address) : m_address(address){
     if (!validate())//TODO: add loging;
         throw std::invalid_argument("Invalid MAC address");
 }
+
+MacAddress::MacAddress(const QString &address): m_address(parseAddress(address)){
+    if (!validate())//TODO: add loging;
+        throw std::invalid_argument("Invalid MAC address");
+}
+
+uint64_t MacAddress::parseAddress(const QString &address) const
+{
+    QRegularExpression re("([0-9A-Fa-f]{2}[:\\-]?){6}");
+    QRegularExpressionMatch match = re.match(address);
+
+    if (!match.hasMatch())
+        throw std::invalid_argument("Invalid MAC address format");
+
+    QString sanitized = address;
+    sanitized.remove(':').remove('-');
+    bool ok; uint64_t mac = sanitized.toULongLong(&ok, 16);
+    if (!ok)
+        throw std::invalid_argument("Failed to parse MAC address");
+    return mac;
+}
