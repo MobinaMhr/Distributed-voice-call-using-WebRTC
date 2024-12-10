3bbe42c0f53b607bca1a85beedb17d962befddc7
Mohammadreza <mrmh13801225@gmail.com>
2024-12-10 16:50:06 +0330

    test macAddress at main

--- CA3/CN_CA_3_Template/src/main.cpp
+++ CA3/CN_CA_3_Template/src/main.cpp
@@ -1,9 +1,20 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include "./MACAddress/testmacaddress.h"
+
+void testMACAddress() {
+    qDebug() << "Testing MacAddress :";
+    TestMacAddress tm ;
+    tm.runAllTests();
+    qDebug() << "----------------------------------";
+}
+
 int
 main(int argv, char* argc[])
 {
     QCoreApplication app(argv, argc);
-    qDebug() << "shit";
+
+    testMACAddress();
+
     return app.exec();
 }
