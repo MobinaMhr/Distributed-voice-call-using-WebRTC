5b6aeb59fcef46d2ff0c5d9dc315d81cec9b8f5a
Mohammadreza <mrmh13801225@gmail.com>
2024-12-10 17:02:07 +0330

    test macAddressGenerator class

--- CA3/CN_CA_3_Template/src/main.cpp
+++ CA3/CN_CA_3_Template/src/main.cpp
@@ -1,11 +1,19 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include "./MACAddress/testmacaddress.h"
+#include "./MACAddress/testmacaddressgenerator.h"
 
 void testMACAddress() {
     qDebug() << "Testing MacAddress :";
     TestMacAddress tm ;
     tm.runAllTests();
+    qDebug() << "----------------------------------";
+}
+
+void testMacAddressGenerator(){
+    qDebug() << "Testing MacAddress Generator :";
+    TestMacAddressGenerator tmg ;
+    tmg.runAllTests();
     qDebug() << "----------------------------------";
 }
 
@@ -15,6 +23,7 @@ main(int argv, char* argc[])
     QCoreApplication app(argv, argc);
 
     testMACAddress();
+    testMacAddressGenerator();
 
     return app.exec();
 }
