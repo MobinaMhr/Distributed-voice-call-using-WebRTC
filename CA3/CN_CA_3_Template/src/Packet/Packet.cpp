92776ff065d52d29ce6c7768c5d11c4f34e06013
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-12-10 23:22:57 +0330

    Implement the Packet module

---
+++ CA3/CN_CA_3_Template/src/Packet/Packet.cpp
@@ -0,0 +1,69 @@
+#include "Packet.h"
+
+using namespace UT;
+
+Packet::Packet(UT::PacketType packetType, UT::PacketControlType controlType, quint32 seqNumber, quint32 waitCycles,
+       quint32 totalCycles, const QString destIP, const QByteArray &payload, const DataLinkHeader &dataLinkHeader,
+       const TCPHeader &tcpHeader, QObject *parent)
+    : QObject(parent), m_packetType(packetType), m_controlType(controlType), m_sequenceNumber(seqNumber),
+    m_waitingCycles(waitCycles), m_totalCycles(totalCycles), m_destinationIP(destIP), m_payload(payload),
+    m_dataLinkHeader(dataLinkHeader), m_tcpHeader(tcpHeader) {}
+
+void Packet::updatePath(const QString& ipAddress) {
+    m_path.append(ipAddress);
+}
+void Packet::increaseWaitingCyclesByOne() {
+    m_waitingCycles = m_waitingCycles + 1;
+}
+void Packet::increaseTotalCyclesByOne() {
+    m_totalCycles = m_totalCycles + 1;
+}
+
+// Getters
+UT::PacketType Packet::packetType() const {
+    return m_packetType;
+}
+UT::PacketControlType Packet::controlType() const {
+    return m_controlType;
+}
+QStringList Packet::path() const {
+    return m_path;
+}
+quint32 Packet::sequenceNumber() const {
+    return m_sequenceNumber;
+}
+quint32 Packet::waitingCycles() const {
+    return m_waitingCycles;
+}
+quint32 Packet::totalCycles() const {
+    return m_totalCycles;
+}
+QByteArray Packet::payload() const {
+    return m_payload;
+}
+const DataLinkHeader& Packet::dataLinkHeader() const {
+    return m_dataLinkHeader;
+}
+const TCPHeader& Packet::tcpHeader() const {
+    return m_tcpHeader;
+}
+
+// Setters
+void Packet::setPacketType(UT::PacketType packetType) {
+    m_packetType = packetType;
+}
+void Packet::setControlType(UT::PacketControlType controlType) {
+    m_controlType = controlType;
+}
+void Packet::setSequenceNumber(quint32 seqNumber) {
+    m_sequenceNumber = seqNumber;
+}
+void Packet::setPayload(const QByteArray& payload) {
+    m_payload = payload;
+}
+void Packet::setDataLinkHeader(const DataLinkHeader& dataLinkHeader) {
+    m_dataLinkHeader = dataLinkHeader;
+}
+void Packet::setTcpHeader(const TCPHeader& tcpHeader) {
+    m_tcpHeader = tcpHeader;
+}
---
+++ CA3/CN_CA_3_Template/src/Packet/Packet.h
@@ -0,0 +1,60 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QByteArray>
+#include "../TCPHeader/tcpheader.h"
+#include "../Globals/globals.h"
+#include "../DataLinkHeader/datalinkheader.h"
+
+class Packet : public QObject {
+    Q_OBJECT
+
+public:
+    explicit Packet(UT::PacketType packetType, UT::PacketControlType controlType, quint32 seqNumber, quint32 waitCycles,
+                    quint32 totalCycles, const QString destIP, const QByteArray &payload,
+                    const DataLinkHeader &dataLinkHeader, const TCPHeader &tcpHeader, QObject *parent);
+
+    void updatePath(const QString& ipAddress);
+    void increaseWaitingCyclesByOne();
+    void increaseTotalCyclesByOne();
+
+    // Getters
+    UT::PacketType packetType() const;
+    UT::PacketControlType controlType() const;
+    QStringList path() const;
+    quint32 sequenceNumber() const;
+    quint32 waitingCycles() const;
+    quint32 totalCycles() const;
+    QString destinationIP() const;
+    QByteArray payload() const;
+    const DataLinkHeader& dataLinkHeader() const;
+    const TCPHeader& tcpHeader() const;
+
+    // Setters
+    void setPacketType(UT::PacketType packetType);
+    void setControlType(UT::PacketControlType controlType);
+    void setSequenceNumber(quint32 seqNumber);
+    void setDestinationIP(QString destIP);
+    void setPayload(const QByteArray &payload);
+    void setDataLinkHeader(const DataLinkHeader &dataLinkHeader);
+    void setTcpHeader(const TCPHeader &tcpHeader);
+
+private:
+    QStringList m_path;
+    UT::PacketType m_packetType;
+    UT::PacketControlType m_controlType;
+
+    quint32 m_sequenceNumber;
+    quint32 m_waitingCycles;
+    quint32 m_totalCycles;
+
+    QString m_destinationIP;
+    QByteArray m_payload;
+    DataLinkHeader m_dataLinkHeader;
+    TCPHeader m_tcpHeader;
+};
+
+#endif // PACKET_H
