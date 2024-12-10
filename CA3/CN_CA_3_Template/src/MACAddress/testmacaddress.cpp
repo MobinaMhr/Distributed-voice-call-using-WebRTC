fbe272818b7f930cd5db909011f05fc2125cf032
Mohammadreza <mrmh13801225@gmail.com>
2024-12-10 16:44:51 +0330

    implement MacAddressTestor testConstructorUint64 method

--- CA3/CN_CA_3_Template/src/MACAddress/testmacaddress.cpp
+++ CA3/CN_CA_3_Template/src/MACAddress/testmacaddress.cpp
@@ -3,3 +3,14 @@
 TestMacAddress::TestMacAddress(QObject *parent) :
     QObject {parent}
 {}
+
+bool TestMacAddress::testConstructorUint64()
+{
+    try {
+        uint64_t address = 0xAABBCCDDEEFF;
+        MacAddress mac(address);
+        return mac.toString() == "aa:bb:cc:dd:ee:ff";
+    } catch (...) {
+        return false;
+    }
+}
