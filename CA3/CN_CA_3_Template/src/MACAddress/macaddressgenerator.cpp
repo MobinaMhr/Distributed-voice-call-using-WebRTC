68007e5e1990c6d33d04360093cc9487958ac87a
Mohammadreza <mrmh13801225@gmail.com>
2024-12-10 16:55:47 +0330

    implement MacAddressGenerator::generateMacAddress method

--- CA3/CN_CA_3_Template/src/MACAddress/macaddressgenerator.cpp
+++ CA3/CN_CA_3_Template/src/MACAddress/macaddressgenerator.cpp
@@ -3,3 +3,10 @@
 MacAddressGenerator::MacAddressGenerator(QObject *parent) :
     QObject {parent}
 {}
+
+MacAddress MacAddressGenerator::generateMacAddress()
+{
+    uint64_t newAddress = generateUniqueAddress();
+    generatedAddresses.insert(newAddress);
+    return MacAddress(newAddress);
+}
