b34545e3ce4e68d4ce83bb1ace6470a0ddc59d07
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-12-11 19:41:44 +0330

    Add header file of EventCoordinator

--- CA3/CN_CA_3_Template/src/EventsCoordinator/EventsCoordinator.h
+++ CA3/CN_CA_3_Template/src/EventsCoordinator/EventsCoordinator.h
@@ -1,41 +1,51 @@
 #ifndef EVENTSCOORDINATOR_H
 #define EVENTSCOORDINATOR_H
 
-#include <chrono>
-
 #include <QObject>
 #include <QThread>
 #include <QTimer>
-
+#include <QVector>
 #include "../DataGenerator/datagenerator.h"
+#include "../Globals/Globals.h"
 
 class EventsCoordinator : public QThread
 {
     Q_OBJECT
 
-    typedef std::chrono::milliseconds Millis;
+    explicit EventsCoordinator(double lambda, int cycleCount, int packetCount, int pcCount, int cycleLength, QThread *parent = nullptr);
 
-    explicit EventsCoordinator(double lambda, int cycleCount, int packetCount, int pcCount, QThread *parent = nullptr);
-
-public:    // constructors
-    ~EventsCoordinator() override = default;
-    static EventsCoordinator *instance(double lambda, int cycleCount, int packetCount, int pcCount, QThread *parent = nullptr);
+public:
+    ~EventsCoordinator() override;
+    static EventsCoordinator *instance(double lambda, int cycleCount, int packetCount, int pcCount, int cycleLength, QThread *parent = nullptr);
     static void               release();
 
-public:    // methods
+public:
 
-public:    // getter and setters
+public:
 
-
+protected:
+    void run() override;
+    void handleCurrentCyclePackets(const int &packetsCount);
 
 public Q_SLOTS:
 
 private Q_SLOTS:
+    void handleTimeout();
 
-private:    // members
+Q_SIGNALS:
+    void packetsReady(std::vector<UT::PacketDetails> packets);
+    void nextTick(int currentCycle);
+    void distributePackets(const QVector<int>& distribution);
+
+private:
     inline static EventsCoordinator *m_self = nullptr;
     DataGenerator *m_dataGenerator;
-    QTimer *m_timer = nullptr;
+    QTimer* m_timer = nullptr;
+    QVector<int> m_distribution;
+    int m_currentCycle = 0;
+    int m_cycleCount;
+    int m_pcCount;
+    int m_cycleLength;
 };
 
 #endif    // EVENTSCOORDINATOR_H
