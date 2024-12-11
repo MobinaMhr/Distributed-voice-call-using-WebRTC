04f4dec26e263f97e6e6585503fdc0ce4af46700
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-12-11 19:42:14 +0330

    Add a nes struct data type for packet detils

--- CA3/CN_CA_3_Template/src/Globals/Globals.h
+++ CA3/CN_CA_3_Template/src/Globals/Globals.h
@@ -1,6 +1,9 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <QObject>
+#include <vector>
+
 namespace UT
 {
 
@@ -43,6 +46,18 @@ enum class TopologyType
     RingStar,
     Torus
 };
+
+struct PacketDetails {
+    int senderID;
+    int receiverID;
+    int packetType;
+
+    PacketDetails(int sender, int receiver, int type)
+        : senderID(sender), receiverID(receiver), packetType(type) {}
+};
 }    // namespace UT
+
+Q_DECLARE_METATYPE(UT::PacketDetails)
+Q_DECLARE_METATYPE(std::vector<UT::PacketDetails>)
 
 #endif    // GLOBALS_H
