79c3e6fa2c334e214d5e1478afcd8367955ff146
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-12-12 17:25:34 +0330

    Add base implementation fo Node class

--- CA3/CN_CA_3_Template/src/Node/node.cpp
+++ CA3/CN_CA_3_Template/src/Node/node.cpp
@@ -1,5 +1,40 @@
-#include "node.h"
+#include "Node.h"
 
-Node::Node(QObject *parent) :
-    QObject {parent}
-{}
+Node::Node(int id, const MacAddress &macAddress, QThread* parent)
+    : QThread(parent), m_id(id), m_macAddress(macAddress), m_name("Node_" + QString::number(id)) {}
+
+Node::~Node() {}
+
+// Node* Node::instance(int id, const MacAddress &macAddress, QThread *parent) {
+//     if (!m_self) {
+//         m_self = new Node(id, macAddress, parent);
+//     }
+//     return m_self;
+// }
+
+void Node::release() {
+    if (m_self) {
+        delete m_self;
+        m_self = nullptr;
+    }
+}
+
+void Node::run() {
+    exec();
+}
+
+int Node::id() const {
+    return m_id;
+}
+
+QString Node::name() const {
+    return m_name;
+}
+
+QString Node::macAddress() const {
+    return m_macAddress.toString();
+}
+
+void Node::processPacket(const QString &packetInfo) {
+    qDebug() << "Node" << m_name << "processing packet:" << packetInfo;
+}
--- CA3/CN_CA_3_Template/src/Node/node.h
+++ CA3/CN_CA_3_Template/src/Node/node.h
@@ -2,15 +2,49 @@
 #define NODE_H
 
 #include <QObject>
+#include <QThread>
+#include <QTimer>
+#include <QVector>
+#include <QRandomGenerator>
+#include <set>
+#include "../IP/IP.h"
+#include "../Globals/Globals.h"
+#include "../MACAddress/macaddress.h"
+#include <QString>
+#include <vector>
 
-class Node : public QObject
+class Node : public QThread
 {
     Q_OBJECT
 
 public:
-    explicit Node(QObject *parent = nullptr);
+    explicit Node(int id, const MacAddress &macAddress, QThread *parent = nullptr);
+    ~Node() override;
+    // static Node *instance(int id, const MacAddress &macAddress, QThread *parent = nullptr);
+    static void release();
+
+    virtual void processPacket(const QString &packetInfo);
+    int id() const;
+    QString name() const;
+    virtual QString ipAddress() const = 0;
+    QString macAddress() const;
+
+protected:
+    void run() override;
+
+public Q_SLOTS:
+
+private Q_SLOTS:
 
 Q_SIGNALS:
+
+private:
+    inline static Node *m_self = nullptr;
+
+    int m_id;
+    MacAddress m_macAddress;
+    QString m_name;
 };
 
-#endif    // NODE_H
+
+#endif // NODE_H
