e9bc0b69d917eab9c2af1c9910743e37c15543a0
MobinaMhr <mobinamehrazar@ut.ac.ir>
2024-12-14 16:08:15 +0330

    Implement the AS class

--- CA3/CN_CA_3_Template/src/AutonomousSystem/autonomoussystem.cpp
+++ CA3/CN_CA_3_Template/src/AutonomousSystem/autonomoussystem.cpp
@@ -1,5 +1,87 @@
 #include "autonomoussystem.h"
 
-AutonomousSystem::AutonomousSystem(QObject *parent) :
-    QObject {parent}
-{}
+#include <QDebug>
+
+AutonomousSystem::AutonomousSystem(int node_count, UT::TopologyType topology_type, QObject *parent) :
+    QObject(parent),
+    m_node_count(node_count),
+    m_isSimulationActive(false),
+    m_topologyType(topology_type),
+    m_topologyController(new TopologyController(this))
+{
+    initializeAS();
+}
+
+AutonomousSystem::~AutonomousSystem() {
+    // delete m_dhcpServer;
+    delete m_topologyController;
+    // delete m_routingProtocol;
+}
+
+void AutonomousSystem::initializeAS() {
+    m_topologyController->initializeTopology(m_topologyType, m_node_count);
+    m_routers = m_topologyController->getCurrentTopology();
+    assignIPAddresses();
+}
+
+void AutonomousSystem::startRoutingProtocol() { /// UT::RoutingProtocolType protocol As input
+    Q_EMIT routingProtocolStarted();
+
+    // if (protocol == UT::RoutingProtocolType::RIP) {
+    //     m_routingProtocol = new RIPProtocol(this);
+    // } else if (protocol == UT::RoutingProtocolType::OSPF) {
+    //     m_routingProtocol = new OSPFProtocol(this);
+    // }
+
+    // if (m_routingProtocol) {
+    //     m_routingProtocol->startProtocol();
+    //     Q_EMIT routingProtocolStarted();
+    // }
+}
+
+void AutonomousSystem::stopRoutingProtocol() {
+    Q_EMIT routingProtocolStopped();
+
+    // if (m_routingProtocol) {
+    //     m_routingProtocol->stopProtocol();
+    //     delete m_routingProtocol;
+    //     m_routingProtocol = nullptr;
+    //     Q_EMIT routingProtocolStopped();
+    // }
+}
+
+void AutonomousSystem::assignIPAddresses() {
+    // m_dhcpServer->assignIPs(m_routers);
+}
+
+void AutonomousSystem::monitorNetwork() {
+    if (m_isSimulationActive) {
+        qDebug() << "Monitoring network...";
+        for (const auto &router : m_routers) {
+            qDebug() << "Router ID:" << router->id() << "has IP:"; // << router->ipAddress();
+        }
+    }
+}
+
+void AutonomousSystem::resetAS() {
+    // m_routers.clear();
+    m_topologyController->resetTopology();
+}
+
+void AutonomousSystem::configAS(UT::TopologyType topology_type) {
+    m_isSimulationActive = false;
+    m_topologyType = topology_type;
+    m_topologyController->changeTopologyTo(m_topologyType);
+}
+
+void AutonomousSystem::startSimulation() {
+    m_isSimulationActive = true;
+    m_topologyController->activateNodes();
+    qDebug() << "Simulation started.";
+}
+
+void AutonomousSystem::stopSimulation() {
+    m_isSimulationActive = false;
+    m_topologyController->deactivateNodes();
+    qDebug() << "Simulation stopped.";
+}
--- CA3/CN_CA_3_Template/src/AutonomousSystem/autonomoussystem.h
+++ CA3/CN_CA_3_Template/src/AutonomousSystem/autonomoussystem.h
@@ -1,16 +1,53 @@
 #ifndef AUTONOMOUSSYSTEM_H
 #define AUTONOMOUSSYSTEM_H
 
+#include "../Topology/topologycontroller.h"
 #include <QObject>
+#include <QVector>
+#include <QSharedPointer>
+#include <QMap>
+// #include "../Router/router.h"
+// #include "../DHCP/dhcpserver.h"
+// #include "../Routing/RoutingProtocol.h"
 
 class AutonomousSystem : public QObject
 {
     Q_OBJECT
 
 public:
-    explicit AutonomousSystem(QObject *parent = nullptr);
+    explicit AutonomousSystem(int node_count, UT::TopologyType topology_type, QObject *parent = nullptr);
+    ~AutonomousSystem() override;
+
+    void startRoutingProtocol(); /// UT::RoutingProtocolType protocol As input
+    void stopRoutingProtocol();
+
+    void assignIPAddresses();
+    void monitorNetwork();
+    void resetAS();
+    void configAS(UT::TopologyType topology_type);
 
 Q_SIGNALS:
+    void routingProtocolStarted();
+    void routingProtocolStopped();
+
+public Q_SLOTS:
+    void startSimulation();
+    void stopSimulation();
+
+private:
+    int                             m_id;
+    int                             m_node_count;
+    bool                            m_isSimulationActive;
+    UT::TopologyType                m_topologyType;
+    QVector<QSharedPointer<Router>> m_routers;
+    QVector<QSharedPointer<Router>> m_as_gateways;
+    QVector<QSharedPointer<Router>> m_userGateways;
+    QVector<QSharedPointer<Router>> m_brokenRouters;
+    QSharedPointer<Router>          m_dhcpServer;
+    TopologyController*             m_topologyController;
+    // RoutingProtocol *m_routingProtocol;
+
+    void initializeAS();
 };
 
 #endif    // AUTONOMOUSSYSTEM_H
